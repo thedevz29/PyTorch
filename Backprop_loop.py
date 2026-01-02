d23ab213bea6803e60dadf4334922f8c681229ac
Atharv Sharma <atharv.sharma2912@gmail.com>
2026-01-02 21:14:13 +0530

    almost finalized

--- Backprop_loop.py
+++ Backprop_loop.py
@@ -1,14 +1,22 @@
 import torch
 
-inp_x = float(input("Enter x: "))
-inp_y = float(input("Enter y (end result): "))
+inp_x = float(input("Enter x: ")) #taking input of x
+inp_y = float(input("Enter y (end result): ")) #taking input of y
 
 x = torch.tensor(inp_x) #input value
 y = torch.tensor(inp_y) #pre determined result (only used for training purpose)
-lr = 0.01
+lr = 0.001
+if x <=50 and y <=75:
+    lr  = 0.001
+elif x <=75 and y<=100:
+    lr = 0.01
+elif x >= 100 and y >=150:
+    lr = 0.1
+else:
+    pass
 w = torch.tensor(1.0, requires_grad=True) #weight
 
-epoch = 0
+epoch = 1
 while True:
     print("Iteration number: ", epoch)
     y_est = w*x
@@ -22,6 +30,7 @@ while True:
         break
     loss.backward()
     print("w.grad = ", w.grad)
+    print("\n\n")
     
     w.data -= lr*w.grad
     w.grad.zero_()
